0ce166603919c49a7c1a3635913a7e3e1efb32d9
Christopher Tse <christopher-tse@outlook.com>
2017-05-19 05:21:16 -0500

    Remove unused constructor and use props to check if session/break and if running

--- src/Timer.js
+++ src/Timer.js
@@ -1,37 +1,30 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class Timer extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            min: Number(props.currMin),
-            sec: Number(props.currSec),
-            
-        }
-
-        this.startTimer = this.startTimer.bind(this)
-        this.decrementMin = this.decrementMin.bind(this)
-        
-    }
 
     render() {
-        let sec = this.state.sec
-        let phrase = ""
+        let session = this.props.isSession
+        let running = this.props.isRunning
+        let sec = this.props.currSec
+        let phrase = " "
 
-        if ( sec < 10 ) {
-            sec = "0"+sec
+        if (running) {
+            if (session) {
+                phrase = "Get Working!"
+            } else {
+                phrase = "Break Time"
+            }
         }
 
-        if (this.state.isSession) {
-            phrase  = "Get Working!"
-        } else {
-            phrase = "Break Time"
+        if (sec < 10) {
+            sec = "0" + sec
         }
-        
+
         return (
             <div className="Timer">
-                <p>{this.state.isRunning ? phrase : "  "}</p>
-                <span id="minute">{this.state.min}:</span><span id="second">{sec}</span>
+                <p>{phrase}</p>
+                <span id="minute">{this.props.currMin}:</span>
+                <span id="second">{sec}</span>
             </div>
         );
     }
