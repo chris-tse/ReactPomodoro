f2e638dfdb3dee69d9da3f63f904044a940d3269
Christopher Tse <christopher-tse@outlook.com>
2017-05-19 05:20:34 -0500

    Removed button handling

--- src/Control.js
+++ src/Control.js
@@ -7,36 +7,13 @@ class Control extends Component {
 			type: props.lengthType,
 			value: props.defaultValue
 		}
-		this.handleMinus = this.handleMinus.bind(this)
-		this.handlePlus = this.handlePlus.bind(this)
-	}
-
-	handleMinus() {
-		let curr = Number(this.state.value)
-		
-		if (curr >= 1) {
-			curr--
-		}
-
-		this.setState({ value: curr })
-		this.props.minDown()
-	}
-
-	handlePlus() {
-		let curr = Number(this.state.value)
-
-		if (curr < 60) {
-			curr++
-		} 
-
-		this.setState({ value: curr })		
 	}
 
 	render() {
 		return (
 			<div className="Control">
 				<p>{this.props.lengthType} Length</p>
-				<a href="#" onClick={this.handleMinus} className="ControlBtn minus">-</a> <span>{this.state.value}</span> <a href="#" onClick={this.handlePlus} className="ControlBtn plus">+</a>
+				<a href="#" onClick={this.props.handleDown} className="ControlBtn minus">-</a> <span>{this.props.value}</span> <a href="#" onClick={this.props.handleUp} className="ControlBtn plus">+</a>
 			</div>
 		);
 	}
